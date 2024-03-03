8b0059d4de37a063cf6644264e9830bbb2d384d4
TimJ0212 <tim.jungk@gmx.de>
2024-03-03 13:08:51 +0100

    v.0

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,24 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { Separator } from '$lib/components/ui/separator';
+	import { ModeWatcher } from 'mode-watcher';
+	import '../app.pcss';
+	import ThemeToggle from './ThemeToggle.svelte';
+</script>
+
+<div class="flex h-screen flex-col">
+	<header class="flex justify-between p-5">
+		<div><a href="/">Dev Tools</a></div>
+		<div class="flex">
+			<Button href="/apps/sprintreview" class="mx-2">Sprint-Review</Button>
+			<Button href="/apps/moscow" class="mx-2">MoSCoW</Button>
+			<Separator orientation="vertical" class="ml-2 mr-4" />
+			<ThemeToggle></ThemeToggle>
+		</div>
+	</header>
+	<slot class="flex-grow">
+		<ModeWatcher />
+	</slot>
+
+	<footer class="mt-auto">Build by TJ</footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <script>
-	import Button from "$lib/components/ui/button/button.svelte";
-
 </script>
-<Button>Click me</Button>
+
+<section
+	class="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20"
+>
+	<div
+		class="text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1]"
+	>
+		Best tools for software developers are now in town.
+	</div>
+	<div class="max-w-[750px] text-center text-lg text-muted-foreground sm:text-xl">
+		Use them for free
+	</div>
+</section>
